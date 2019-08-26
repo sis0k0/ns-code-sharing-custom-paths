620bdacada5f5f6330e6a14664a7cd52b9d630cb
sis0k0 <stanimira.vlaeva@gmail.com>
2019-08-26 11:27:52 +0300

    refactor: change TS imports to use `@app` instead of `@src/app`

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,9 +1,9 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 
-import { AppRoutingModule } from '@src/app/app-routing.module';
-import { AppComponent } from '@src/app/app.component';
-import { HomeComponent } from '@src/app/home/home.component';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+import { HomeComponent } from '@app/home/home.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
